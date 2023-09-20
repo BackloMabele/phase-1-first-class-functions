00d183ff422382be3986ed70a8a43d6af254aef1
BackloMabele <backlo.mabele@student.moringaschool.com>
2023-09-20 13:41:52 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 function receivesAFunction (callback) {
     return callback();
   }
+  var fn = returnsANamedFunction();
   function returnsANamedFunction() {
     var fn = function() {
         console.log("This is a named function");
     };
     return fn;
+}
+var fn = returnsAnAnonymousFunction();
+function returnsAnAnonymousFunction() {
+    return function() {
+        console.log("This one is an anonymous function")
+    };
 }
